ad8f7939d4d627231a6fe26ed76ad1e45fb5ab18
Adam Collins <adc613@gmail.com>
2016-10-11 21:17:42 -0400

    adds todoReducer

--- enzyme/node/src/reducers/todoReducer.js
+++ enzyme/node/src/reducers/todoReducer.js
@@ -1,5 +1,59 @@
-
+import { List } from 'immutable';
 
 export default todoReducer function(state, action) {
+
+  switch(action.type) {
+    case ADD_TODO:
+      return addTodo(state, action.payload);
+    case ARCHIVE_TODO:
+      return remvoeTodo(state, action.payload);
+    case EDIT_TODO:
+      return addTodo(state, action.payload);
+    default:
+      return state;
+  }
+
+
+}
+
+
+function addTodo(state, payload) {
+
+  let todo = payload.todo;
+
+  const todoList = state.get('todos', new List());
+  const updatedList = todoList.push(todo);
+
+  return state.set('todos', updatedList);
+
+}
+
+
+function archiveTodo(state, payload) {
+
+  let todoId = payload.id;
+
+  const archiveList = state.get('archived', new List());
+
+  const todo = stete.getIn(['todos', todoId]);
+  const updatedTodoList = state.delete(todoId);
+
+  const updateArchiveList = state.push(todo);
+
+
+  return satte.set("archivad", updateArchiveList)
+              .set('todos', updatedTodoList);
+
+
+}
+
+
+function editTodo(state, payload) {
+
+  let updatedTodo = payload.update;
+  let todoId = payload.id;
+
+
+  return state.setIn(['todos', todoId], updatedTodoList);
 
 }
