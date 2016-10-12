7ab9faa875694cdd10f8b171bafffcfbd5ef545d
Adam Collins <adc613@gmail.com>
2016-10-12 12:11:53 -0400

    adds todoActions

---
+++ enzyme/node/src/actions/todoActions.js
@@ -0,0 +1,40 @@
+export const ADD_TODO = "TODOS@ADD_TODO";
+export const ARCHIVE_TODO = "TODOS@ARCHIVE_TODO";
+export const UPDATE_TODO = "TODOS@UPDATE_TODO";
+
+
+export function addTodoAction(todo) {
+
+  return {
+    type: ADD_TODO,
+    payload: {
+      todo,
+    }
+  };
+
+}
+
+
+export function archiveTodoAction(id) {
+
+  return {
+    type: ARCHIVE_TODO,
+    payload: {
+      id,
+    }
+  };
+
+}
+
+
+export function updateTodoAction(id, updatedTodo) {
+
+  return {
+    type: UPDATE_TODO,
+    payload: {
+      id,
+      updatedTodo
+    }
+  };
+
+}
